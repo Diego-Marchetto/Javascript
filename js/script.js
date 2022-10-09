b041540eb0c83c87e4239bc65c6aa7e388007fbd
Diego Marchetto <diegomarchetto@hotmail.com>
2022-10-09 12:53:33 -0300

    Segunda entrega Javascript

--- js/script.js
+++ js/script.js
@@ -3,45 +3,58 @@
 //3-SUMAR LO COMPRADO A UN CARRITO DE COMPRAS PARA LUEGO PODER MOSTRARLE AL USUARIO, RESTAR STOCK SEGUN LO QUE VA PIDIENDO EL USUARIO
 //4-CONFIRMAR COMPRA PREGUNTANDO METODO DE PAGO Y HACIENDO DESCUENTOS
 
-let nombreProductoA = 'CPU'
-let precioProductoA = 10000
-let stockProductoA = 27
-
-let nombreProductoB = 'Monitor'
-let precioProductoB = 5000
-let stockProductoB = 78
-
-let precioTotal = 0 //VALOR TOTAL DE LA COMPRA
-
-let comprasA = 0 //Cantidad de compras del producto A
-let comprasB = 0 //Cantidad de compras del producto B
-
-let descuento = 0.15 //Descuento transferencia bancaria
-
-function mostrarCarrito(){ //Muestra el carrito del usuario
-    if((comprasA>=1)&&(comprasB==0)){ //No me gusta para nada este metodo, pero no se me ocurrio otra manera de verificar varios productos para mostrar o no dentro del ALERT.
-        alert("TU CARRITO: \n\n-" + comprasA + " CPU, valor: $" + (comprasA * precioProductoA) + "\n\n VALOR TOTAL: $" + precioTotal)
-    }
-    else if((comprasA>=1)&&(comprasB>=1)){
-        alert("TU CARRITO: \n\n-" + comprasA + " CPU, valor: $" + (comprasA * precioProductoA) + ". \n-" + comprasB + " Monitores, valor: $" + (comprasB * precioProductoB) + "\n\n VALOR TOTAL: $" + precioTotal)
-    }
-    else if((comprasA==0)&&(comprasB>=1)){
-        alert("TU CARRITO: \n\n-" + comprasB + " Monitores, valor: $" + (comprasB * precioProductoB) + "\n\n VALOR TOTAL: $" + precioTotal)
-    }
-    else{
-        alert("Tu carrito esta vacio.")
+function Producto(nombre, precio, stock){ //OBJETOS
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock || 0;
+    this.sumarStock = function(cantidad){
+        this.stock += cantidad
     }
 }
 
+let productoA = new Producto("CPU", 5350, 20) //Genero objetos
+let productoB = new Producto("RAM", 2330, 89)
+let productoC = new Producto("MOTHER", 4780, 22)
+let productoD = new Producto("SSD", 1500, 70)
+let productoE = new Producto("PLACA DE VIDEO", 11700, 10)
+
+let listaProductos = [productoA, productoB, productoC, productoD, productoE] //Armar array con los productos
+
+let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0) //Filtrar en un array los productos con stock
+
+let listaNombres = listaProductosConStock.map((prod) => prod.nombre) //Mapear el nombre de los productos con stock a un array nuevo
+
+let precioTotal = 0 //VALOR TOTAL DE LA COMPRA
+
+let comprasA = 0
+let comprasB = 0
+let comprasC = 0
+let comprasD = 0
+let comprasE = 0
+
+let descuento = 0.15 //Descuento transferencia bancaria
+
 function calcularStock(cantidad, stockActual, precio, nombre){
     if(cantidad <= stockActual){ 
-        if(stockActual==stockProductoA){//verifico cual es la variable a la que tengo que descontar
-            stockProductoA -= cantidad
-            comprasA += cantidad //agregado de variable para sumar X cantidad de compras de X producto para mostrar en carrito
+        if(stockActual==productoA.stock){//verifico cual es la variable a la que tengo que descontar
+            productoA.stock -= cantidad
+            comprasA += 1
         }
-        else if(stockActual==stockProductoB){
-            stockProductoB -= cantidad
-            comprasB += cantidad //agregado de variable para sumar X cantidad de compras de X producto para mostrar en carrito
+        else if(stockActual==productoB.stock){
+            productoB.stock -= cantidad
+            comprasB += 1
+        }
+        else if(stockActual==productoC.stock){
+            productoC.stock -= cantidad
+            comprasC += 1
+        }
+        else if(stockActual==productoD.stock){
+            productoD.stock -= cantidad
+            comprasD += 1
+        }
+        else if(stockActual==productoE.stock){
+            productoE.stock -= cantidad
+            comprasE += 1
         }
         precioTotal += (cantidad * precio) //Actualizo el precio total de la compra
         alert("Agregaste " + cantidad + " " + nombre + " al carrito!")
@@ -53,7 +66,7 @@ function calcularStock(cantidad, stockActual, precio, nombre){
 
 function metodoPago(){
     if((comprasA>0)||(comprasB>0)){ //Si el carrito no esta vacio
-        let metodoPago = parseInt(prompt("Ingrese el metodo de pago que va a utilizar: \n\n1-MercadoPago\n2-Transferencia bancaria (15% de descuento)"))
+        let metodoPago = parseInt(prompt("Ingrese el metodo de pago que va a utilizar: \n\n1-MercadoPago\n2-Transferencia bancaria (15% de descuento) \n\nTotal a pagar: " + precioTotal))
         if(metodoPago==1){ //Finalizo compra y reseteo variables
             alert("Muchas gracias por tu compra, compraste un total de $" + precioTotal + ".")
             reset()
@@ -73,31 +86,56 @@ function metodoPago(){
 
 function reset(){
     precioTotal = 0
-    comprasA = 0
-    comprasB = 0
 }
 
 alert("Bienvenido a nuestra tienda Gamer") //Dar bienvenida
 
-let menu = parseInt(prompt("MENU: \n1-Comprar \n2-Mostrar carrito\n3-Confirmar compra \n0-Salir")) //Mostrar el menu al usuario
+let menu = parseInt(prompt("MENU: \n1-Comprar \n2-Confirmar compra \n0-Salir")) //Mostrar el menu al usuario
 
 while(menu != 0){ //While para navegar en el menu principal
     if(menu==1){
-            let producto = parseInt(prompt("Estos son algunos de nuestros productos: \n1-CPU -STOCK: " + stockProductoA + " \n2-Monitor -STOCK: " + stockProductoB + "\n\nIngrese el producto que desea comprar con su numeracion correspondiente.\n\n0-Volver al Menu"))
+            let producto = parseInt(prompt("Estos son nuestros productos: \n - " + listaNombres.join("\n - ") + "\n\nIngrese el producto que desea comprar con su numeracion correspondiente.\n\n0-Volver al Menu"))
         while(producto != 0){ //While para navegar sobre el menu COMPRAS            
             if(producto == 1){ //Comprar producto A
-                if(stockProductoA>0){
-                    let cantidadCPU = parseInt(prompt("Contamos con un stock de " + stockProductoA + " " + nombreProductoA + ", cuantas desea comprar?:"))
-                    calcularStock(cantidadCPU, stockProductoA, precioProductoA, nombreProductoA)
+                if(productoA.stock>0){
+                    let cantidadCPU = parseInt(prompt("Contamos con un stock de " + productoA.stock + " " + productoA.nombre + ", cuantas desea comprar?:"))
+                    calcularStock(cantidadCPU, productoA.stock, productoA.precio, productoA.nombre)
                 }
                 else{
                     alert("No contamos con stock de ese producto.")
                 }
             }
             else if(producto == 2){ //Comprar producto B
-                if(stockProductoB>0){
-                let cantidadMonitor = parseInt(prompt("Contamos con un stock de " + stockProductoB + " " + nombreProductoB + ", cuantas desea comprar?:"))
-                calcularStock(cantidadMonitor, stockProductoB, precioProductoB, nombreProductoB)
+                if(productoB.stock>0){
+                let cantidadRAM = parseInt(prompt("Contamos con un stock de " + productoB.stock + " " + productoB.nombre + ", cuantas desea comprar?:"))
+                calcularStock(cantidadRAM, productoB.stock, productoB.precio, productoB.nombre)
+                }
+                else{
+                    alert("No contamos con stock de ese producto.")
+                }
+            }
+            else if(producto == 3){ //Comprar producto C
+                if(productoC.stock>0){
+                let cantidadMOTHER = parseInt(prompt("Contamos con un stock de " + productoC.stock + " " + productoC.nombre + ", cuantas desea comprar?:"))
+                calcularStock(cantidadMOTHER, productoC.stock, productoC.precio, productoC.nombre)
+                }
+                else{
+                    alert("No contamos con stock de ese producto.")
+                }
+            }
+            else if(producto == 4){ //Comprar producto D
+                if(productoD.stock>0){
+                let cantidadSSD = parseInt(prompt("Contamos con un stock de " + productoD.stock + " " + productoD.nombre + ", cuantas desea comprar?:"))
+                calcularStock(cantidadSSD, productoD.stock, productoD.precio, productoD.nombre)
+                }
+                else{
+                    alert("No contamos con stock de ese producto.")
+                }
+            }
+            else if(producto == 5){ //Comprar producto E
+                if(productoE.stock>0){
+                let cantidadGrafica = parseInt(prompt("Contamos con un stock de " + productoE.stock + " " + productoE.nombre + ", cuantas desea comprar?:"))
+                calcularStock(cantidadGrafica, productoE.stock, productoE.precio, productoE.nombre)
                 }
                 else{
                     alert("No contamos con stock de ese producto.")
@@ -106,20 +144,16 @@ while(menu != 0){ //While para navegar en el menu principal
             else{ //No existe el producto
                 alert("No tenemos ese producto a la venta.")
             }
-            producto = parseInt(prompt("Que otro producto desea comprar?: \n1-CPU -STOCK: " + stockProductoA + " \n2-Monitor -STOCK: " + stockProductoB + "\n\nIngrese el producto que desea comprar con su numeracion correspondiente.\n\n0-Volver al Menu"))
+            producto = parseInt(prompt("Estos son nuestros productos: \n - " + listaNombres.join("\n - ") + "\n\nIngrese el producto que desea comprar con su numeracion correspondiente.\n\n0-Volver al Menu"))
         }
     }
-    else if (menu==2){ //Si el usuario elige la opcion 2 (MENU PRINCIPAL) le muestra el carrito
-        mostrarCarrito()
-    }
-
-    else if (menu==3){ //Confirmar compra, elegir metodo de pago
+    else if (menu==2){ //Confirmar compra, elegir metodo de pago
         metodoPago()
     } 
     else{ //Se ingresa un valor no valido
         alert("La opcion es invalida, vuelva a seleccionar por favor.")
     }
-    menu = parseInt(prompt("MENU: \n1-Comprar \n2-Mostrar carrito \n3-Confirmar comprar \n0-Salir"))
+    menu = parseInt(prompt("MENU: \n1-Comprar \n2-Confirmar comprar \n0-Salir"))
 }
 
 alert("Gracias por visitar nuestra tienda!") //Despedir al usuario
