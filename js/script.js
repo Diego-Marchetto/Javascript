b272303742b8f1a81f71cc65d515ed8080aaae47
Diego Marchetto <diegomarchetto@hotmail.com>
2022-10-10 23:06:54 -0300

    Implementacion de DOM al proyecto, carrito de compras

--- js/script.js
+++ js/script.js
@@ -3,22 +3,49 @@
 //3-SUMAR LO COMPRADO A UN CARRITO DE COMPRAS PARA LUEGO PODER MOSTRARLE AL USUARIO, RESTAR STOCK SEGUN LO QUE VA PIDIENDO EL USUARIO
 //4-CONFIRMAR COMPRA PREGUNTANDO METODO DE PAGO Y HACIENDO DESCUENTOS
 
-function Producto(nombre, precio, stock){ //OBJETOS
+function Producto(nombre, precio, stock, img, categoria, id){ //OBJETOS
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock || 0;
+    this.img = img;
+    this.categoria = categoria;
+    this.id = id;
+    this.restarStock = function(cantidad){
+        if(this.stock > 0){
+            this.stock -= cantidad
+        }
+    }
     this.sumarStock = function(cantidad){
         this.stock += cantidad
     }
 }
 
-let productoA = new Producto("CPU", 5350, 20) //Genero objetos
-let productoB = new Producto("RAM", 2330, 89)
-let productoC = new Producto("MOTHER", 4780, 22)
-let productoD = new Producto("SSD", 1500, 70)
-let productoE = new Producto("PLACA DE VIDEO", 11700, 10)
+let productoA = new Producto("Ryzen 5 5600G", 5350, 2, "./imgs/cpu.jpg", "Procesadores", 1)//Genero objetos
+let productoB = new Producto("Ryzen 5 5600X", 5350, 20, "./imgs/cpu.jpg", "Procesadores", 2)
+let productoC = new Producto("Ryzen 7 5700G", 5350, 20, "./imgs/cpu7.jpg", "Procesadores", 3)
+let productoD = new Producto("Ryzen 7 5700X ", 5350, 20, "./imgs/cpu7.jpg", "Procesadores", 4) 
 
-let listaProductos = [productoA, productoB, productoC, productoD, productoE] //Armar array con los productos
+let productoE = new Producto("RAM", 2330, 89, "./imgs/ram.jpg", "Memorias RAM", 5)
+let productoF = new Producto("RAM", 2330, 89, "./imgs/ram.jpg", "Memorias RAM", 6)
+let productoG = new Producto("RAM", 2330, 89, "./imgs/ram.jpg", "Memorias RAM", 7)
+let productoH = new Producto("RAM", 2330, 89, "./imgs/ram.jpg", "Memorias RAM", 8)
+
+let productoI = new Producto("MOTHER", 4780, 22, "./imgs/mother.jpg", "Motherboards", 9)
+let productoJ = new Producto("MOTHER", 4780, 22, "./imgs/mother.jpg", "Motherboards", 10)
+let productoK = new Producto("MOTHER", 4780, 22, "./imgs/mother.jpg", "Motherboards", 11)
+let productoM = new Producto("MOTHER", 4780, 22, "./imgs/mother.jpg", "Motherboards", 12)
+
+let productoN = new Producto("SSD", 1500, 70, "./imgs/ssd.jpg", "Discos Solidos", 13)
+let productoO = new Producto("SSD", 1500, 70, "./imgs/ssd.jpg", "Discos Solidos", 14)
+let productoP = new Producto("SSD", 1500, 70, "./imgs/ssd.jpg", "Discos Solidos", 15)
+let productoQ = new Producto("SSD", 1500, 70, "./imgs/ssd.jpg", "Discos Solidos", 16)
+
+let productoR = new Producto("PLACA DE VIDEO", 11700, 10, "./imgs/gpu.jpg", "Placas de Video", 17)
+let productoS = new Producto("PLACA DE VIDEO", 11700, 10, "./imgs/gpu.jpg", "Placas de Video", 18)
+let productoT = new Producto("PLACA DE VIDEO", 11700, 10, "./imgs/gpu.jpg", "Placas de Video", 19)
+let productoU = new Producto("PLACA DE VIDEO", 11700, 10, "./imgs/gpu.jpg", "Placas de Video", 20)
+
+let listaProductos = [productoA, productoB, productoC, productoD, productoE, productoF, productoG, productoH, productoI, productoJ, productoK, productoM, productoN, productoO, productoP, productoQ, productoR, productoS, productoT, productoU] //Armar array con los productos
 
 let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0) //Filtrar en un array los productos con stock
 
@@ -26,134 +53,110 @@ let listaNombres = listaProductosConStock.map((prod) => prod.nombre) //Mapear el
 
 let precioTotal = 0 //VALOR TOTAL DE LA COMPRA
 
-let comprasA = 0
-let comprasB = 0
-let comprasC = 0
-let comprasD = 0
-let comprasE = 0
-
 let descuento = 0.15 //Descuento transferencia bancaria
 
-function calcularStock(cantidad, stockActual, precio, nombre){
-    if(cantidad <= stockActual){ 
-        if(stockActual==productoA.stock){//verifico cual es la variable a la que tengo que descontar
-            productoA.stock -= cantidad
-            comprasA += 1
-        }
-        else if(stockActual==productoB.stock){
-            productoB.stock -= cantidad
-            comprasB += 1
-        }
-        else if(stockActual==productoC.stock){
-            productoC.stock -= cantidad
-            comprasC += 1
-        }
-        else if(stockActual==productoD.stock){
-            productoD.stock -= cantidad
-            comprasD += 1
-        }
-        else if(stockActual==productoE.stock){
-            productoE.stock -= cantidad
-            comprasE += 1
-        }
-        precioTotal += (cantidad * precio) //Actualizo el precio total de la compra
-        alert("Agregaste " + cantidad + " " + nombre + " al carrito!")
-    }
-    else{
-        alert("No contamos con stock suficiente, actualmente tenemos " + stockActual + " unidades de este producto.")
-    }
-}
 
-function metodoPago(){
-    if((comprasA>0)||(comprasB>0)){ //Si el carrito no esta vacio
-        let metodoPago = parseInt(prompt("Ingrese el metodo de pago que va a utilizar: \n\n1-MercadoPago\n2-Transferencia bancaria (15% de descuento) \n\nTotal a pagar: " + precioTotal))
-        if(metodoPago==1){ //Finalizo compra y reseteo variables
-            alert("Muchas gracias por tu compra, compraste un total de $" + precioTotal + ".")
-            reset()
-        }
-        else if(metodoPago==2){ //Se calcula el precio total menos el 15% y reseteo variables
-            alert("Beneficio por pagar mediante transferencia bancaria:\n\n Antes: $" + precioTotal + "\n Ahora: $" + (precioTotal - (precioTotal * descuento))  + "\n\nMuchas gracias por comprar en nuestra tienda!")
-            reset()
+
+let catalogo = document.getElementById("lista")
+let filtrarCpu = document.getElementById("filCPU")
+let filtrarRam = document.getElementById("filRAM")
+let filtrarSsd = document.getElementById("filSSD")
+let filtrarMother = document.getElementById("filMOT")
+let filtrarGpu = document.getElementById("filGPU")
+let all = document.getElementById("all")
+let sumarCantidad = document.getElementById("cant")
+let mostrarCarrito = document.getElementById("listaCarrito")
+
+function render(lista) {
+    catalogo.innerHTML = ""
+
+    for(const prod of lista){
+
+        let card = document.createElement("div")
+
+        card.className = "item"
+
+        if(prod.stock > 0){
+            card.innerHTML = `<img src="${prod.img}" alt="${prod.nombre}" alt=""><h2 class="titulo">${prod.nombre}</h2><p>Precio: $${prod.precio}</p><button id="${prod.id}">SUMAR AL CARRITO</button>`
         }
         else{
-            alert("Opcion invalida.")
+            card.innerHTML = `<img src="${prod.img}" alt="${prod.nombre}" alt=""><h2 class="titulo">${prod.nombre}</h2><p>Precio: $${prod.precio}</p><button id="${prod.id}" class="nostock">SIN STOCK</button>`
         }
+
+        catalogo.append(card)
+
+        const boton = document.getElementById(`${prod.id}`)
+        
+        boton.addEventListener("click", ()=> agregarCarrito(prod.id, prod.stock))
+    }
+}
+render(listaProductosConStock)
+
+function renderCarrito(listaCarrito) {
+    mostrarCarrito.innerHTML = ""
+
+    for(const prod of listaCarrito){
+
+        let carro = document.createElement("div")
+
+        carro.className = "item"
+
+        carro.innerHTML = `<img src="${prod.img}" alt="${prod.nombre}" alt=""><h2 class="titulo">${prod.nombre}</h2><p>Precio: $${prod.precio}</p><button id="borrar${prod.id}">ELIMINAR</button>`
+
+        mostrarCarrito.append(carro)
+
+        const botonBorrar = document.getElementById(`borrar${prod.id}`)
+        
+        botonBorrar.addEventListener("click", ()=> borrarDelCarrito(prod.id))
+    }
+}
+
+filtrarCpu.addEventListener("click", ()=>render(listaProductos.filter((prod)=>prod.categoria == filtrarCpu.textContent)))
+filtrarRam.addEventListener("click", ()=>render(listaProductos.filter((prod)=>prod.categoria == filtrarRam.textContent)))
+filtrarSsd.addEventListener("click", ()=>render(listaProductos.filter((prod)=>prod.categoria == filtrarSsd.textContent)))
+filtrarMother.addEventListener("click", ()=>render(listaProductos.filter((prod)=>prod.categoria == filtrarMother.textContent)))
+filtrarGpu.addEventListener("click", ()=>render(listaProductos.filter((prod)=>prod.categoria == filtrarGpu.textContent)))
+all.addEventListener("click", ()=>{render(listaProductosConStock)})
+
+
+let carrito = []
+let sumarCarrito = 0
+
+const agregarCarrito = (prodId, stock) => {
+    const newItem = listaProductosConStock.find((prod) => prod.id === prodId)
+    if(stock > 0){
+        carrito.push(newItem)
+        let cantidad = 1
+        newItem.restarStock(cantidad)
+
+        sumarCarrito += 1
+        sumarCantidad.textContent = sumarCarrito
+    
+        console.log(carrito)
+        render(listaProductosConStock)
+        renderCarrito(carrito)
     }
     else{
-        alert("Tu carrito esta vacio.") //Si el carrito esta vacio no lo deja confirmar ninguna compra
-    } 
-}
-
-function reset(){
-    precioTotal = 0
-}
-
-alert("Bienvenido a nuestra tienda Gamer") //Dar bienvenida
-
-let menu = parseInt(prompt("MENU: \n1-Comprar \n2-Confirmar compra \n0-Salir")) //Mostrar el menu al usuario
-
-while(menu != 0){ //While para navegar en el menu principal
-    if(menu==1){
-            let producto = parseInt(prompt("Estos son nuestros productos: \n - " + listaNombres.join("\n - ") + "\n\nIngrese el producto que desea comprar con su numeracion correspondiente.\n\n0-Volver al Menu"))
-        while(producto != 0){ //While para navegar sobre el menu COMPRAS            
-            if(producto == 1){ //Comprar producto A
-                if(productoA.stock>0){
-                    let cantidadCPU = parseInt(prompt("Contamos con un stock de " + productoA.stock + " " + productoA.nombre + ", cuantas desea comprar?:"))
-                    calcularStock(cantidadCPU, productoA.stock, productoA.precio, productoA.nombre)
-                }
-                else{
-                    alert("No contamos con stock de ese producto.")
-                }
-            }
-            else if(producto == 2){ //Comprar producto B
-                if(productoB.stock>0){
-                let cantidadRAM = parseInt(prompt("Contamos con un stock de " + productoB.stock + " " + productoB.nombre + ", cuantas desea comprar?:"))
-                calcularStock(cantidadRAM, productoB.stock, productoB.precio, productoB.nombre)
-                }
-                else{
-                    alert("No contamos con stock de ese producto.")
-                }
-            }
-            else if(producto == 3){ //Comprar producto C
-                if(productoC.stock>0){
-                let cantidadMOTHER = parseInt(prompt("Contamos con un stock de " + productoC.stock + " " + productoC.nombre + ", cuantas desea comprar?:"))
-                calcularStock(cantidadMOTHER, productoC.stock, productoC.precio, productoC.nombre)
-                }
-                else{
-                    alert("No contamos con stock de ese producto.")
-                }
-            }
-            else if(producto == 4){ //Comprar producto D
-                if(productoD.stock>0){
-                let cantidadSSD = parseInt(prompt("Contamos con un stock de " + productoD.stock + " " + productoD.nombre + ", cuantas desea comprar?:"))
-                calcularStock(cantidadSSD, productoD.stock, productoD.precio, productoD.nombre)
-                }
-                else{
-                    alert("No contamos con stock de ese producto.")
-                }
-            }
-            else if(producto == 5){ //Comprar producto E
-                if(productoE.stock>0){
-                let cantidadGrafica = parseInt(prompt("Contamos con un stock de " + productoE.stock + " " + productoE.nombre + ", cuantas desea comprar?:"))
-                calcularStock(cantidadGrafica, productoE.stock, productoE.precio, productoE.nombre)
-                }
-                else{
-                    alert("No contamos con stock de ese producto.")
-                }
-            }
-            else{ //No existe el producto
-                alert("No tenemos ese producto a la venta.")
-            }
-            producto = parseInt(prompt("Estos son nuestros productos: \n - " + listaNombres.join("\n - ") + "\n\nIngrese el producto que desea comprar con su numeracion correspondiente.\n\n0-Volver al Menu"))
-        }
+        console.log("No hay stock")
     }
-    else if (menu==2){ //Confirmar compra, elegir metodo de pago
-        metodoPago()
-    } 
-    else{ //Se ingresa un valor no valido
-        alert("La opcion es invalida, vuelva a seleccionar por favor.")
-    }
-    menu = parseInt(prompt("MENU: \n1-Comprar \n2-Confirmar comprar \n0-Salir"))
 }
 
-alert("Gracias por visitar nuestra tienda!") //Despedir al usuario
+const borrarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    item.sumarStock(1)
+
+    sumarCarrito -= 1
+    sumarCantidad.textContent = sumarCarrito
+
+    console.log(carrito)
+    render(listaProductosConStock)
+    renderCarrito(carrito)
+}
+
+
+
+
+
+
